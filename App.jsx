672549d893a28aa5b0cb032a10d293267187d525
Andrew Stilliard <andrew.stilliard@gmail.com>
2025-09-29 15:21:36 +0100

    test(normalise): added unit test for phone normalisation

--- App.jsx
+++ App.jsx
@@ -1,36 +1,6 @@
 import { h } from 'preact';
 import { signal } from '@preact/signals';
-
-function normalizeUKPhone(number) {
-  let num = number.replace(/[^\d+]/g, '');
-  if (num.startsWith('0')) {
-    num = '+44' + num.slice(1);
-  }
-  else if (num.startsWith('44')) {
-    num = '+44' + num.slice(2);
-  }
-  else if (num.startsWith('+44')) {
-    // do nothing
-  }
-  else if (num.startsWith('+')) {
-    // do nothing
-  }
-  else {
-    num = '+44' + num;
-  }
-
-  // make sure the number starts with +447 as all UK mobile numbers do
-  if (!num.startsWith('+44')) {
-    return null;
-  }
-
-  // also length should be 13 characters for UK mobile numbers
-  if (num.length !== 13) {
-    return null;
-  }
-
-  return num;
-}
+import { normalizeUKPhone } from './lib/normalize-uk-phone.js';
 
 const input = signal('');
 const output = signal('');
